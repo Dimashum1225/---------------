eae2b666c5d643f23b84bced79337efafba96ca2
Dimashum1225 <sumatbaev7@gmail.com>
2025-04-01 13:53:42 +0500

    refactor time

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function updateCountdown() {
-    const targetDate = new Date('2025-05-16T00:00:00').getTime();
+    const targetDate = new Date('2025-05-16T00:17:10').getTime();
     const now = new Date().getTime();
     const difference = targetDate - now;
 
